93e4215406b803ccc2c1b71369493846232e7779
egororachyov <egororachyov5@gmail.com>
2018-12-15 14:52:06 +0300

    Setup project, add sh script for setting

---
+++ ClientServer/setup.sh
@@ -0,0 +1,1 @@
+mvn archetype:generate -DinteractiveMode=false -DarchetypeGroupId=org.openjdk.jmh -DarchetypeArtifactId=jmh-java-benchmark-archetype -DgroupId=Test -DartifactId=ClientServer -Dversion=1.0
