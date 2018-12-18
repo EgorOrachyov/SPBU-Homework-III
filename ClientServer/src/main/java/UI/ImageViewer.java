47099cf2da66a9f75769036aa0253c3ac71afcee
egororachyov <egororachyov5@gmail.com>
2018-12-18 22:52:17 +0300

    Add view and save screens for UI

--- ClientServer/src/main/java/UI/ImageViewer.java
+++ ClientServer/src/main/java/UI/ImageViewer.java
@@ -4,9 +4,18 @@ import Filter.Image;
 
 import javax.swing.*;
 
-public class ImageViewer extends JWindow {
+public class ImageViewer extends JFrame {
 
     public ImageViewer(Image toView) {
+
+        add(new JLabel(new ImageIcon(toView.getData())));
+
+        setTitle("View");
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize(toView.getWidth(), toView.getHeight());
+        setLocationRelativeTo(null);
+        setVisible(true);
 
     }
 
