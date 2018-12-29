c3d72aad257e5b17b6cbe6387ab20f94da6fca13
Egor Orachyov <31863304+EgorOrachyov@users.noreply.github.com>
2018-12-29 20:10:34 +0300

    Rename sh script file

--- BlurBenchmark/mvn_proj_init.sh
+++
@@ -1,1 +0,0 @@
-mvn archetype:generate -DinteractiveMode=false -DarchetypeGroupId=org.openjdk.jmh -DarchetypeArtifactId=jmh-java-benchmark-archetype -DgroupId=Test -DartifactId=BlurProfiling -Dversion=1.0
